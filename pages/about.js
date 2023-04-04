86c8885754b81f55ff29fcdfe5df31259cd69a13
Tulkas1992 <vr24355@gmail.com>
2023-04-04 16:59:35 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
+
+
+const about = () => {
+  return (<>
+    <Head>
+      <title>Esto es about</title>
+    </Head>
+    <main className={styles.main}>
+
+    <div>about</div>
+    </main>
+    </>
+  )
+}
+
+export default about;
